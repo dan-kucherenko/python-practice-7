80c64bc51f5609e0b70a58914303394aae5d6bb3
dan-kucherenko <daniil.ukraine@gmail.com>
2024-03-12 20:36:09 +0200

    Add docstrings to functions + add exception handling

--- app/io/input.py
+++ app/io/input.py
@@ -2,16 +2,47 @@ import pandas as pd
 
 
 def get_user_input():
-    return input("Enter user's input:")
+    """
+    Prompt the user to enter input and return the input entered by the user.
+
+    Returns:
+        str: The input entered by the user.
+    """
+    return input("Enter user's input: ")
 
 
 def read_input_file(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
+    """
+    Read lines from a file and return them as a list of strings.
 
-    return lines
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        list: A list of strings representing the lines read from the file.
+    """
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+        return lines
+    except FileNotFoundError:
+        return "File not found"
 
 
-def read_input_file_panda(filename):
-    dataframe = pd.read_csv(filename)
-    return dataframe
+def read_input_file_pandas(filename):
+    """
+    Read a file using pandas and return the contents as a DataFrame.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        pandas.DataFrame or None: A DataFrame containing the contents of the file,
+                                  or None if the file is not found.
+    """
+    try:
+        dataframe = pd.read_csv(filename)
+        return dataframe
+    except FileNotFoundError:
+        print(f'CSV file with path {filename} can\'t be found')
+        return None
--- app/io/output.py
+++ app/io/output.py
@@ -1,7 +1,23 @@
 def print_to_console(text):
+    """
+    Print the provided text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+    """
     print(text)
 
 
 def write_to_file(filename, text):
-    with open(filename, 'w') as file:
-        file.write(text)
+    """
+    Write the provided text to the specified file.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to be written to the file.
+    """
+    try:
+        with open(filename, 'w') as file:
+            file.write(text)
+    except FileNotFoundError:
+        print(f'Error writing to file {filename}')
